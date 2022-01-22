145ed12f32c23687bc94c16f306b0a3be97d8b43
vuducanh0802 <vuducanh1708@gmail.com>
2022-01-22 23:37:15 +0800

    Dockerfile + main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+from matplotlib import pyplot as plt
+
+def main():
+    while(True):
+        randomlist = []
+        for i in range(0,10):
+            n = random.randint(1,30)
+            randomlist.append(n)
+        plt.plot(randomlist)
+        plt.show()
+        user_input = input('Do you want another list (y/[n])? ')
+        if user_input != 'y':
+            break
+    
+
+if __name__ == '__main__':
+    main()
